56bd03af125d1dee93af07e5fa5d46917cf26b3b
Mymoza <marie-philippe.gill@hotmail.ca>
2016-06-04 22:27:57 -0400

    Fix typo de Albert

--- slack_bot.js
+++ slack_bot.js
@@ -237,7 +237,7 @@ controller.hears(['shutdown'], 'direct_message,direct_mention,mention', function
     });
 });
 
-*/
+
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
     'direct_message,direct_mention,mention', function(bot, message) {
 
